ac508c26121a6f87082e3c1ff59290cd41d7ff75
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-08-03 21:30:15 +0200

    file-scoped namespaces fix csproj

--- src/NeverMuffle.cs
+++ src/NeverMuffle.cs
@@ -2,29 +2,28 @@
 using Modding;
 using UnityEngine;
 
-namespace NeverMuffle
+namespace NeverMuffle;
+
+public class NeverMuffle : Mod
 {
-    public class NeverMuffle : Mod
+    public override void Initialize()
     {
-        public override void Initialize()
-        {
-            On.GameManager.Start += OnGameManagerStart;
+        On.GameManager.Start += OnGameManagerStart;
 
-            ChangeMuffle(Object.FindObjectOfType<AudioManager>());
-        }
+        ChangeMuffle(Object.FindObjectOfType<AudioManager>());
+    }
 
-        private void OnGameManagerStart(On.GameManager.orig_Start orig, GameManager self)
-        {
-            orig(self);
+    private void OnGameManagerStart(On.GameManager.orig_Start orig, GameManager self)
+    {
+        orig(self);
 
-            ChangeMuffle(Object.FindObjectOfType<AudioManager>());
-        }
+        ChangeMuffle(Object.FindObjectOfType<AudioManager>());
+    }
 
-        private void ChangeMuffle(AudioManager self)
-        {
-            var hdeFsm = self.gameObject.LocateMyFSM("Hero Damaged Effects");
-            var tlState = hdeFsm.Fsm.GetState("To Lowpass");
-            tlState.Actions = new FsmStateAction[0];
-        }
+    private void ChangeMuffle(AudioManager self)
+    {
+        var hdeFsm = self.gameObject.LocateMyFSM("Hero Damaged Effects");
+        var tlState = hdeFsm.Fsm.GetState("To Lowpass");
+        tlState.Actions = new FsmStateAction[0];
     }
 }
